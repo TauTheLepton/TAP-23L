33f86179021edee3159300c7b4cf82f1a856f82d
Mateusz Palczuk <mateusz.palczuk@gmail.com>
2023-06-01 19:58:00 +0200

    Improvements in DMC ana

--- dmc_ana.m
+++ dmc_ana.m
@@ -1,123 +1,147 @@
-function error = dmc_ana(params)
-    % params=[70, 6, 50];
+% function error = dmc_ana(params)
+close all;
+    h = 15.56;
+    T = 40.42;
+    T_out = T;
+    params=[10, 6, 50];
     %% set parameters
     use_plot = true; % for optimalization with algorithm should be 'false'
     use_z = 0; %0-brak zaklocen, 1- zaklocenie skokowe, 2-zaklocenie sinusoidalne,3-zaklocenie skokowe z szumem
     dB = 40; %wielkosc szumu: 10 - duza, 20 - srednia, 40 - mala
     calc_z = false; %czy probuje kontrowac zaklocenie (mzp*dzp)
     % dmc params
-    n = params(1);
-    nu = params(2);
+    N = params(1);
+    Nu = params(2);
     lambda = params(3);
+    N = 5;
+    Nu = 3;
+    lambda = 7;
+    psi = 0.5;
     % goal values
-    len = 600;
-    yzad = ones(len, 1);
-    yzad(1:len/4) = 0;
+    Tp = 10;
+    t = 1:Tp:Tp*1000;
+    t = t';
+    len = size(t, 1);
+    err = zeros(len, 2);
+    yzad = zeros(len, 2);
+    yzad(len/4:end,1) = 2;
+    yzad(len/4:end,2) = 2;
     
     %% read s and d
-    temp = load('S_u.mat');
-    s = temp.S_u;
-    d = 170;
-    temp = load('S_z.mat');
-    sz = temp.S_z;
-    dz = 150;
+    % temp = load('S_u.mat');
+    D = 7;
+    shFh = xd1(1:D,1,1); %TODO: pobierać z pliku
+    sToutFh = xd1(1:D,2,1);
+    shFcin = xd2(1:D,1,2);
+    sToutFcin = xd2(1:D,2,2);
+    S = zeros(2,2,D);
+    S(1,1,:) = shFh;
+    S(1,2,:) = shFcin;
+    S(2,1,:) = sToutFh;
+    S(2,2,:) = sToutFcin;
+    % S = [[shFh shFcin]; [sToutFh sToutFcin]]
+    % temp = load('S_z.mat');
+    % sz = temp.S_z;
+    % dz = 150;
     %% calculate offline
     % generate mp
-    mp = zeros(n, d-1);
-    for i=1:n
-        for j=1:d-1
-            first = i+j;
-            if first > d
-                first = d;
+    mp = zeros(2*N, 2*(D-1));
+    for i=1:N
+        for j=1:D-1
+            idx = i+j;
+            if idx > D
+                idx = D;
             end
-            mp(i, j) = s(first) - s(j);
+            mp(2*i-1:2*i, 2*j-1:2*j) = S(:,:,idx) - S(:,:,j);
         end
     end
     % generate mzp
-    mzp = zeros(n, dz);
-    for i=1:dz
-        if i==1
-           mzp(1:n,1)=sz(1:n);
-        else
-           mzp(1:n,i)=sz(i:n+i-1)-sz(i-1);
-        end
-    end
+    % mzp = zeros(n, dz);
+    % for i=1:dz
+    %     if i==1
+    %        mzp(1:n,1)=sz(1:n);
+    %     else
+    %        mzp(1:n,i)=sz(i:n+i-1)-sz(i-1);
+    %     end
+    % end
     % generate m
-    m = zeros(n, nu);
-    for i=1:n
-        for j=1:nu
+    m = zeros(2*N, 2*Nu);
+    for i=1:N
+        for j=1:Nu
             if i >= j
                 idx = i-j+1;
-                if idx > d
-                    idx = d;
+                if idx > D
+                    idx = D;
                 end
-                m(i, j) = s(idx);
+                m(2*i-1:2*i, 2*j-1:2*j) = S(:,:,idx);
             end
         end
     end
     % calculate K
-    K = (m'*m+(lambda*eye(nu)))\m';
+    K = (m'*(psi*eye(N*2))*m+(lambda*eye(Nu*2)))\m';
     %% main loop, online calculations
-    dup = zeros(d-1, 1);
-    dzp = zeros(dz,1);
-    u = zeros(len, 1);
-    y = zeros(len, 1);
-    z = zeros(len, 1);
-    if use_z == 1
-        z(350:len,1)=1;
-    end
-    if use_z == 2
-        z(350:len, 1) = sin(linspace(0,1,len-350+1)*20)/3; % sinusoidal disturbance
-    end
-    if use_z == 3
-        z(350:len,1)=1;
-        z=awgn(z, dB);
-    end
+    dup = zeros(2*(D-1), 2);
+    % dzp = zeros(dz,1);
+    u = zeros(len, 2);
+    y = zeros(len, 2);
+    % z = zeros(len, 1);
+    % if use_z == 1
+    %     z(350:len,1)=1;
+    % end
+    % if use_z == 2
+    %     z(350:len, 1) = sin(linspace(0,1,len-350+1)*20)/3; % sinusoidal disturbance
+    % end
+    % if use_z == 3
+    %     z(350:len,1)=1;
+    %     z=awgn(z, dB);
+    % end
     
-    err = zeros(len, 1);
-    for k=13:len
+    for k=2:len
         %% process simulation
-        y(k)=symulacja_obiektu8y_p2(u(k-6),u(k-7),z(k-1),z(k-2),y(k-1),y(k-2));
+        yvect = lsim(Gz, u(1:k,:), t(1:k));
+        last = size(yvect,1);
+        y(k,:) = yvect(last,:);
         %% calculate error
-        err(k) = yzad(k) - y(k);
+        err(k,:) = yzad(k,:) - y(k,:);
         %% calculate control values
         % calculate dup
-        for i=1:d-1
+        for i=1:D-1
             if k-i-1 >= 1
-                dup(i) = u(k-i) - u(k-i-1);
+                dup(i, :) = u(k-i, :) - u(k-i-1, :);
             end
         end
         % calculate y0
-        y0 = (y(k)*ones(n, 1)) + mp*dup;
-        if calc_z
-            y0=y0+mzp*dzp;
-        end
-        % calculate du
-        du = K*(yzad(k)*ones(n, 1) - y0);
+        y0 = y(k,:) + mp*dup;
+        du = K*(yzad(k,:) - y0);
         % change u increment to u value
-        u(k) = u(k-1) + du(1);
-        %calculate dzp
-        for i = dz:-1:2
-            dzp(i)=dzp(i-1);
-        end
-        dzp(1)=z(k)-z(k-1);
+        u(k,:) = u(k-1,:) + du(1,:);
     end
     %% calculate error
-    error = err' * err;
+    error = [0; 0];
+    error(1) = err(:,1)' * err(:,1);
+    error(2) = err(:,2)'*err(:,2);
     %% plot
     if use_plot
-        subplot(3, 1, 1)
+        subplot(4, 1, 1)
         hold on
-        plot(yzad)
-        plot(y)
+        plot(yzad(:,1)+h)
+        plot(y(:,1)+h)
         hold off
         legend('yzad', 'y')
     
-        subplot(3, 1, 2)
-        plot(u)
+        subplot(4, 1, 2)
+        plot(u(:,1))
         legend('u')
-        subplot(3, 1, 3)
-        plot(z)
-        legend('z')
+
+        subplot(4, 1, 3)
+        hold on
+        plot(yzad(:,2)+T_out)
+        plot(y(:,2)+T_out)
+        hold off
+        legend('yzad', 'y')
+
+        subplot(4, 1, 4)
+        plot(u(:,2))
+        legend('u')
     end
-end
+% end
